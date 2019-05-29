a3b1160657eb0d5368dd465e03a6c7cfb8b00cc9
DenmarFel <denmarfeliciano@gmail.com>
2019-05-28 19:35:38 -0700

    Fixed python location

--- nps.py
+++ nps.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from npsAPI import * 
 from forms import ParkSearchForm
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 #Home Page
 @app.route('/', methods=['GET','POST'])
